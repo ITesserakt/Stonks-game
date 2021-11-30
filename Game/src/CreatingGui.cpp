998b7aeae75e60c02d6af629756725958a533e57
Seba <vsevolod.kolotilin@gmail.com>
2021-11-30 21:17:57 +0300

    CreatingGui.cpp is more readable

--- Game/src/CreatingGui.cpp
+++ Game/src/CreatingGui.cpp
@@ -15,15 +15,15 @@
 #include "widgets/MessageBox.h"
 
 void setupMainMenu(WorldState &state, Canvases &scenes) {
-    auto label1 = std::make_shared<Label>("game name", "STONKS GAME\n");
+    auto label = std::make_shared<Label>("game name", "STONKS GAME\n");
     auto butPl = std::make_shared<Button>("play", 0, SceneChangeCommand(state, scenes[SceneNames::GameField]));
     auto butSt = std::make_shared<Button>("settings", 1, SceneChangeCommand(state, scenes[SceneNames::Settings]));
     auto butGd = std::make_shared<Button>("guide", 2, SceneChangeCommand(state, scenes[SceneNames::Guide]));
     auto butStat = std::make_shared<Button>("statistics", 3, SceneChangeCommand(state, scenes[SceneNames::Statistics]));
     auto butQ = std::make_shared<Button>("quit", 4, ShutdownCommand(state));
 
-    label1->turnOn(COLOR_YELLOW);
-    scenes[SceneNames::MainMenu]->bind(label1);
+    label->turnOn(COLOR_YELLOW);
+    scenes[SceneNames::MainMenu]->bind(label);
     scenes[SceneNames::MainMenu]->bind(butPl);
     scenes[SceneNames::MainMenu]->bind(butSt);
     scenes[SceneNames::MainMenu]->bind(butGd);
@@ -32,14 +32,14 @@ void setupMainMenu(WorldState &state, Canvases &scenes) {
 }
 
 void setupGameField(WorldState &state, Canvases &scenes) {
-    auto label2 = std::make_shared<Label>("stocks", "Game field\n");
+    auto label = std::make_shared<Label>("stocks", "Game field\n");
+    label->turnOn(COLOR_YELLOW);
+    scenes[SceneNames::GameField]->bind(label);
+
     auto balance = std::make_shared<Label>("Money Amount", "Balance: \n");
     balance->setRegularNameChanging(std::chrono::milliseconds(100), [&](){
               return std::string("Balance: ") + std::to_string(state.getPlayer().getBalance());
            });
-
-    label2->turnOn(COLOR_YELLOW);
-    scenes[SceneNames::GameField]->bind(label2);
     scenes[SceneNames::GameField]->bind(balance);
 
     for (int i = 0; i < state.getWorld().getSlots().size(); i++) {
@@ -50,14 +50,14 @@ void setupGameField(WorldState &state, Canvases &scenes) {
 
     auto winMessage = std::make_shared<MessageBox>("Win Message", "You have win!");
     winMessage->hide(true);
-
     scenes[SceneNames::GameField]->bind(winMessage);
+
 }
 
 void setupInventory(WorldState &state, Canvases &scenes) {
     auto label = std::make_shared<Label>("inventory", "Inventory\n");
-    scenes[SceneNames::Inventory]->bind(label);
     label->turnOn(COLOR_YELLOW);
+    scenes[SceneNames::Inventory]->bind(label);
 
     for (unsigned long i = 0; i < state.getPlayer().getInventorySize(); i++) {
         auto sale = std::make_shared<Sale>(i, Command::noop());
@@ -95,20 +95,10 @@ void setupSettings(WorldState &state, Canvases &scenes) {
     auto label = std::make_shared<Label>("guide", "Settings\n");
     label->turnOn(COLOR_YELLOW);
 
-    auto restartMessage = std::make_shared<MessageBox>("configRestart",
-                                                       "Do you want to restart game\n"
-                                                       "to apply config changes?", SpecialPosition::Special);
+    auto butRt = std::make_shared<Button>("reset\nconfig", 0);
 
-    auto yes = std::make_shared<Button>("yes", 3);
-    yes->applyAction(StateCommand::fromFunction(state, [](WorldState &s){
-        std::__fs::filesystem::remove("../share/config.json");
-    }).then(ShutdownCommand(state)));
-    auto no = std::make_shared<Button>("no", 4);
-
-    auto butStMn = std::make_shared<Button>("back", 2, SceneChangeCommand(state, scenes[SceneNames::MainMenu]));
     std::vector<std::shared_ptr<Label>> levelNames;
-    auto levels = std::make_shared<Button>(
-            "difficulties", 1,
+    auto levels = std::make_shared<Button>("difficulties", 1,
             StateCommand::fromFunction(state, [isLevelsExpanded = false](WorldState &s) mutable {
                 isLevelsExpanded = !isLevelsExpanded;
                 for (int i = 0; i < Config::presets.size(); i++) {
@@ -125,7 +115,8 @@ void setupSettings(WorldState &state, Canvases &scenes) {
         i++;
     }
 
-    auto butRt = std::make_shared<Button>("reset\nconfig", 0);
+    auto butStMn = std::make_shared<Button>("back", 2, SceneChangeCommand(state, scenes[SceneNames::MainMenu]));
+
     auto initialGroup = std::make_shared<Group>("Initial");
     initialGroup->bind(label);
     initialGroup->bind(butRt);
@@ -134,6 +125,16 @@ void setupSettings(WorldState &state, Canvases &scenes) {
         //initialGroup->bind(item);
     initialGroup->bind(butStMn);
     scenes[SceneNames::Settings]->bind(initialGroup);
+
+    auto restartMessage = std::make_shared<MessageBox>("configRestart",
+                                                       "Do you want to restart game\n"
+                                                       "to apply config changes?", SpecialPosition::Special);
+
+    auto yes = std::make_shared<Button>("yes", 3);
+    yes->applyAction(StateCommand::fromFunction(state, [](WorldState &s){
+                         std::__fs::filesystem::remove("../share/config.json");
+                     }).then(ShutdownCommand(state)));
+    auto no = std::make_shared<Button>("no", 4);
 
     auto groupForRestart = std::make_shared<Group>("Restart");
     groupForRestart->bind(restartMessage);
@@ -163,7 +164,6 @@ void setupSettings(WorldState &state, Canvases &scenes) {
 
 void setupStatistics(WorldState &state, Canvases &scenes) {
     auto leftGroup = std::make_shared<Group>("Management");
-    auto rightGroup = std::make_shared<Group>("Statistics");
 
     auto label = std::make_shared<Label>("Statistics", "STONKS-TISTICS\n");
     label->turnOn(COLOR_YELLOW);
@@ -172,6 +172,7 @@ void setupStatistics(WorldState &state, Canvases &scenes) {
     leftGroup->bind(label);
     leftGroup->bind(butStMn);
 
+    auto rightGroup = std::make_shared<Group>("Statistics");
     auto amountOfPurchases = std::make_shared<Label>("Stat1", "Amount of purchases: 0\n");
     amountOfPurchases->setRegularNameChanging(std::chrono::seconds {1}, [&]() -> std::string{
              std::random_device dev;
